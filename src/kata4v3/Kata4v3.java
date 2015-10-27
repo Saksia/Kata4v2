59296beebb560c869b82e784e992ec2d906e6859
usuario <usuario@10.22.143.7>
2015-10-27 10:02:04 +0000

    versión final v4 - bytes

--- src/kata4v3/Kata4v3.java
+++ src/kata4v3/Kata4v3.java
@@ -12,15 +12,17 @@ import java.io.OutputStream;
 
 public class Kata4v3 {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String from = "C:\\Users\\usuario\\Desktop\\pruebakata.pdf";
-        String to = "C:\\Users\\usuario\\Desktop\\pruebakata(1).pdf";
+        String from = "C:\\Users\\usuario\\Desktop\\hola.txt";
+        String to = "C:\\Users\\usuario\\Desktop\\hola (1).txt";
         InputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
         OutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
         long start = System.currentTimeMillis();
+        byte [] buffer = new byte [1024];
         while(true){
             int read = input.read();
             if(read < 0) break;
             output.write(read);
+            output.write(buffer);
         }
         System.out.println(System.currentTimeMillis()-start);
         input.close();
